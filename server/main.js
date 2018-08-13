b4fc81c5c52e0f8f28179445fc3770999da68a78
Gilles Kbidy <gilles.kbidy@l3t.com>
2018-08-13 10:54:31 -0700

    dirty prototype

--- server/main.js
+++ server/main.js
@@ -3,4 +3,17 @@ import '../imports/api/tasks.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  // Support for playing D&D: Roll 3d6 for dexterity.
+  Accounts.onCreateUser((options, user) => {
+    const customizedUser = Object.assign({
+      dexterity: 'test',
+    }, user);
+
+    // We still want the default hook's 'profile' behavior.
+    if (options.profile) {
+      customizedUser.profile = options.profile;
+    }
+
+    return customizedUser;
+  });
 });
